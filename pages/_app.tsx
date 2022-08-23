a498b129ae538afef9bbd0d916b450b8b765ccb8
Renan.Correa <renan.rcp16@gmail.com>
2022-08-23 09:29:42 -0300

    first commit

---
+++ pages/_app.tsx
@@ -0,0 +1,16 @@
+import '../styles/tailwind.css';
+import Layout from '../components/Layout';
+import { ApolloProvider } from '@apollo/client';
+import apolloClient from '../lib/apollo';
+
+function MyApp({ Component, pageProps }) {
+	return (
+		<ApolloProvider client={apolloClient}>
+			<Layout>
+				<Component {...pageProps} />
+			</Layout>
+		</ApolloProvider>
+	);
+}
+
+export default MyApp;
